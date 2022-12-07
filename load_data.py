bc793f06f0dc24a3707a83bb18b2b350d5811282
GIACOMO GALLIANO <giacomo.galliano@contentwise.com>
2022-12-07 17:56:27 +0100

    Add existing project files to Git

---
+++ load_data.py
@@ -0,0 +1,19 @@
+import numpy
+
+def load_data(file_name):
+    label_list = []
+    data_list = []
+
+    with open(file_name) as file:
+        for line in file:
+            try:
+                features_list = line.split(',')
+                features = numpy.array(features_list[0:11], dtype='float32').reshape(-1,1)
+                label = int(features_list[-1])
+                data_list.append(features)
+                label_list.append(label)
+            except:
+                print('Error while reading file!')
+                pass
+
+    return numpy.hstack(data_list), numpy.array(labelsList, dtype=numpy.int32)
---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy
+import load_data
+
+
+if __name__ == '__main__':
+
+    ## TR_data: Training Data  
+    ## TE_data: Evaluation Data
+    ## TR_labels: Training Labels
+    ## TE_labels: Evaluation Labels
+
+    TR_data, TR_labels
